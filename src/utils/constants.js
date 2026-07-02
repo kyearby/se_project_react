bf5568aabc1e0746042285c88b59c86942e0c549
Kya <kyearby1@gmail.com>
2026-07-01 22:12:04 -0500

    Styling Cards

---
+++ src/utils/constants.js
@@ -0,0 +1,37 @@
+export const defaultClothingItems = [
+  {
+    _id: 1,
+    name: "Boot",
+    weather: "cold",
+    link:
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Boot.png?etag=0953a2ea59f1c6ebc832fabacdc9c70e",
+  },
+  {
+    _id: 2,
+    name: "Cap",
+    weather: "hot",
+    link:
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Cap.png?etag=f3dad389b22909cafa73cff9f9a3d591",
+  },
+  {
+    _id: 3,
+    name: "Coat",
+    weather: "cold",
+    link:
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Coat.png?etag=298717ed89d5e40b1954a1831ae0bdd4",
+  },
+  {
+    _id: 4,
+    name: "Dress",
+    weather: "hot",
+    link:
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Dress.png?etag=1f9cd32a311ab139cab43494883720bf",
+  },
+  {
+    _id: 5,
+    name: "Hoodie",
+    weather: "cold",
+    link:
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Hoodie.png?etag=5f52451d0958ccb1016c78a45603a4e8",
+  },
+];
