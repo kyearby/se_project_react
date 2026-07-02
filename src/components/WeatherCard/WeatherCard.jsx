718c5192346c82c48530c18fc4ae76965a9d5cd1
Kya <kyearby1@gmail.com>
2026-07-01 21:00:55 -0500

    WeatherCard Basics

---
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -0,0 +1,10 @@
+import './WeatherCard.css';
+import sunny from  '../../images/sunny.png';
+
+function WeatherCard(){
+    return <section className="weather-card">
+        <p className="weather-card__temp">75 &deg; F</p>
+        <img src={sunny} alt="sunny" className="weather-card__image" />
+    </section>;
+}
+export default WeatherCard;
